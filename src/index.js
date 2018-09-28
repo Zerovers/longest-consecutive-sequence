f92cde95ba582dddc605289191db440cfb69139d
Zerovers <Zeromon19941994@mail.ru>
2018-09-28 14:56:28 +0300

    emplemented longestConsecutiveLength

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  array.sort((a, b) => a - b);
+  let k = 0;
+  let stack = [];
+  let consecutiveLength = 0;
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] == array[i - 1]) {
+      continue;
+    }
+    if (array[i] == array[i - 1] + 1) {
+      stack.push(array[i]);
+      //console.log(stack);
+
+    } else {
+
+      stack = [];
+      stack.push(array[i]);
+      //console.log(stack);
+    }
+    if (stack.length > consecutiveLength) {
+      consecutiveLength = stack.length;
+    }
+  }
+  return consecutiveLength;
 }
